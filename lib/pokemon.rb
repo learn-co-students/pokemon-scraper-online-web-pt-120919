5f33cdb3fadd7cbf9810dfa5f6220b1aa33ac0cc
lydzb123 <lydiabaik@gmail.com>
2020-03-02 20:51:32 -0800

    finds a pokemon from the database by their id number and returns a new Pokemon object

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,6 +10,15 @@ class Pokemon
         VALUES (?, ?)", name, type)
     end
     
+    def self.find(id, db) #what is db referencing here?
+        pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [id])
+        new_pokemon = self.new(pokemon)
+        new_pokemon.id = pokemon[0][0]
+        new_pokemon.name = pokemon[0][1]
+        new_pokemon.type = pokemon[0][2]
+        new_pokemon
+    end
+
 
 
 end
