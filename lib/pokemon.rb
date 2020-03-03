a1bc02b363497873b72e53662743aeca8e4ac157
lydzb123 <lydiabaik@gmail.com>
2020-03-02 20:44:11 -0800

    saves an instance of a pokemon with the correct id

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,5 +5,11 @@ class Pokemon
     def initialize(keywords)
     end
 
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type)
+        VALUES (?, ?)", name, type)
+    end
+    
+
 
 end
